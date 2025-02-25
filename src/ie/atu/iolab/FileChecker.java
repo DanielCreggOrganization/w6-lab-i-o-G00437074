cb2a8809bd9a9d8b43a0c12f9d30d42572ea5b82
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2025-02-25 09:01:15 +0000

    Exercise 1

---
+++ src/ie/atu/iolab/FileChecker.java
@@ -0,0 +1,17 @@
+package ie.atu.iolab;
+
+import java.io.File;
+
+public class FileChecker {
+    public static void main(String[] args) {
+        // Define the file to check
+        File file = new File("resources/output.txt");
+
+        // Check if the file exists
+        if (file.exists()) {
+            System.out.println("File found: " + file.getAbsolutePath());
+        } else {
+            System.out.println("File not found: " + file.getAbsolutePath());
+        }
+    }
+}
---
+++ src/ie/atu/iolab/Main.java
@@ -0,0 +1,9 @@
+package ie.atu.iolab;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java I/O Lab Setup Complete!");
+    }
+
+}
